5bb5eac6f59ac2586b27d394905a8cc6c9d4986c
woodcol <fengmm521@gmail.com>
2016-12-08 16:50:42 +0800

    add https post upload file
    add https post upload file

--- httpsServer.py
+++ httpsServer.py
@@ -5,14 +5,26 @@
 # @Version : $Id$
 #https服务器
 import os
-import BaseHTTPServer, SimpleHTTPServer
 import ssl
-from SimpleHTTPServer import SimpleHTTPRequestHandler as HTTPSHandler
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SocketServer import ThreadingMixIn
+import threading
+import logging
+import cgi
+import time
+import posixpath
+import urllib
+import sys
+import shutil
+import mimetypes
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 
 curdir = '.'
-
-class myHandler(HTTPSHandler):  
-      
+class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):  
     	print self.path
         if self.path=="/":  
@@ -52,57 +64,231 @@ class myHandler(HTTPSHandler):
         except IOError:  
             self.send_error(404,'File Not Found: %s' % self.path)  
   
-    def do_POST(self):  
-    	print 'do post'
-        logging.warning(self.headers)  
-        form = cgi.FieldStorage(  
-            fp=self.rfile,  
-            headers=self.headers,  
-            environ={'REQUEST_METHOD':'POST',  
-                    'CONTENT_TYPE':self.headers['Content-Type'],  
-                    })  
-  
-        file_name = self.get_data_string()  
-        path_name = '%s/%s.log' % (RES_FILE_DIR,file_name)  
-        fwrite = open(path_name,'a')  
-  
-        fwrite.write("name=%s\n" % form.getvalue("name",""))  
-        fwrite.write("addr=%s\n" % form.getvalue("addr",""))  
-        fwrite.close()  
-  
-        self.send_response(200)  
-        self.end_headers()  
-        self.wfile.write("Thanks for you post")  
-  
-    def get_data_string(self):  
-        now = time.time()  
-        clock_now = time.localtime(now)  
-        cur_time = list(clock_now)  
-        date_string = "%d-%d-%d-%d-%d-%d" % (cur_time[0],  
-                cur_time[1],cur_time[2],cur_time[3],cur_time[4],cur_time[5])  
-        return date_string  
-print 'https server is running....'
+    def do_POST(self):
+        """Serve a POST request."""
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST',
+                     'CONTENT_TYPE': self.headers['Content-Type'],
+                     })
+        filename = form['file'].filename
+        print filename
+        path = self.translate_path(self.path)
+        print path
+        filepath = os.path.join(path, filename)
+        print filepath
+        if os.path.exists(filepath):
+            self.log_error('File %s exist!', filename)
+            filepath += '.new'
+
+        with open(filepath, 'wb') as f:
+            f.write(form['file'].value)
+
+        self.do_GET()
+
+    def do_HEAD(self):
+        """Serve a HEAD request."""
+        f = self.send_head()
+        if f:
+            f.close()
+
+    def send_head(self):
+        """Common code for GET and HEAD commands.
+
+        This sends the response code and MIME headers.
+
+        Return value is either a file object (which has to be copied
+        to the outputfile by the caller unless the command was HEAD,
+        and must be closed by the caller under all circumstances), or
+        None, in which case the caller has nothing further to do.
+
+        """
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            if not self.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(301)
+                self.send_header("Location", self.path + "/")
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            # Always read in binary mode. Opening files in text mode may cause
+            # newline translations, making the actual size of the content
+            # transmitted *less* than the content-length!
+            f = open(path, 'rb')
+        except IOError:
+            self.send_error(404, "File not found")
+            return None
+        self.send_response(200)
+        self.send_header("Content-type", ctype)
+        fs = os.fstat(f.fileno())
+        self.send_header("Content-Length", str(fs[6]))
+        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+        self.end_headers()
+        return f
+
+    def list_directory(self, path):
+        """Helper to produce a directory listing (absent index.html).
+
+        Return value is either a file object, or None (indicating an
+        error).  In either case, the headers are sent, making the
+        interface the same as for send_head().
+
+        """
+        try:
+            list = os.listdir(path)
+        except os.error:
+            self.send_error(404, "No permission to list directory")
+            return None
+        list.sort(key=lambda a: a.lower())
+        f = StringIO()
+        displaypath = cgi.escape(urllib.unquote(self.path))
+        f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+        f.write("<html>\n<title>Directory listing for %s</title>\n" % displaypath)
+        f.write("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath)
+        f.write('''<form action="" enctype="multipart/form-data" method="post">\n
+                    <input name="file" type="file" />
+                    <input value="upload" type="submit" />
+                </form>''')
+        f.write("<hr>\n<ul>\n")
+        for name in list:
+            if name.startswith('.'):
+                continue
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+            if os.path.islink(fullname):
+                displayname = name + "@"
+                # Note: a link to a directory displays with @ and links with /
+            f.write('<li><a href="%s">%s</a>\n'
+                    % (urllib.quote(linkname), cgi.escape(displayname)))
+        f.write("</ul>\n<hr>\n</body>\n</html>\n")
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        encoding = sys.getfilesystemencoding()
+        self.send_header("Content-type", "text/html; charset=%s" % encoding)
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        return f
+
+    def translate_path(self, path):
+        """Translate a /-separated PATH to the local filename syntax.
+
+        Components that mean special things to the local file system
+        (e.g. drive or directory names) are ignored.  (XXX They should
+        probably be diagnosed.)
+
+        """
+        # abandon query parameters
+        path = path.split('?',1)[0]
+        path = path.split('#',1)[0]
+        path = posixpath.normpath(urllib.unquote(path))
+        words = path.split('/')
+        words = filter(None, words)
+        path = os.getcwd()
+        for word in words:
+            drive, word = os.path.splitdrive(word)
+            head, word = os.path.split(word)
+            if word in (os.curdir, os.pardir): continue
+            path = os.path.join(path, word)
+        return path
+
+    def copyfile(self, source, outputfile):
+        """Copy all data between two file objects.
+
+        The SOURCE argument is a file object open for reading
+        (or anything with a read() method) and the DESTINATION
+        argument is a file object open for writing (or
+        anything with a write() method).
+
+        The only reason for overriding this would be to change
+        the block size or perhaps to replace newlines by CRLF
+        -- note however that this the default server uses this
+        to copy binary data as well.
+
+        """
+        shutil.copyfileobj(source, outputfile)
+
+    def log_error(self, format, *args):
+        """Log an error.
+
+        Display error message in red color.
+        """
+
+        format = '\033[0;31m' + format + '\033[0m'
+        self.log_message(format, *args)
+
+    def guess_type(self, path):
+        """Guess the type of a file.
+
+        Argument is a PATH (a filename).
+
+        Return value is a string of the form type/subtype,
+        usable for a MIME Content-type header.
+
+        The default implementation looks the file's extension
+        up in the table self.extensions_map, using application/octet-stream
+        as a default; however it would be permissible (if
+        slow) to look inside the data to make a better guess.
+
+        """
+
+        base, ext = posixpath.splitext(path)
+        if ext in self.extensions_map:
+            return self.extensions_map[ext]
+        ext = ext.lower()
+        if ext in self.extensions_map:
+            return self.extensions_map[ext]
+        else:
+            return self.extensions_map['']
+
+    if not mimetypes.inited:
+        mimetypes.init() # try to read system mime.types
+    extensions_map = mimetypes.types_map.copy()
+    extensions_map.update({
+        '': 'application/octet-stream', # Default
+        '.py': 'text/plain',
+        '.c': 'text/plain',
+        '.h': 'text/plain',
+        })
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
 
 
 serverAddr = ('127.0.0.1',4443)
 
 
-httpd = BaseHTTPServer.HTTPServer(serverAddr, myHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='server.pem', server_side=True)
-httpd.serve_forever()
-
-
-# except KeyboardInterrupt:  
-#     print '^C received, shutting down the web server'  
-#     server.socket.close() 
+if __name__ == '__main__':
+    server = ThreadedHTTPServer(serverAddr, myHandler)
+    print 'https server is running....'
+    print 'Starting server, use <Ctrl-C> to stop'
+    server.socket = ssl.wrap_socket (server.socket, certfile='server.pem', server_side=True)
+    server.serve_forever()
 
 
 # # 生成rsa密钥
-# $ openssl genrsa -des3 -out server.key 1024
+# $ openssl genrsa -des3 -out server.key 2048
 # # 去除掉密钥文件保护密码
 # $ openssl rsa -in server.key -out server.key
 # # 生成ca对应的csr文件
 # $ openssl req -new -key server.key -out server.csr
 # # 自签名
-# $ openssl x509 -req -days 1024 -in server.csr -signkey server.key -out server.crt
+# $ openssl x509 -req -days 2048 -in server.csr -signkey server.key -out server.crt
 # $ cat server.crt server.key > server.pem
